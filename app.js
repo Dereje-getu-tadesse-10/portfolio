822768b9c98c44d7c64b5dc86974857cf3dc2424
Dereje <getutadessedereje@icloud.com>
2022-05-01 14:49:33 +0200

    debut responsive

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function animationNavbar() {
     tl.to('nav',{
         y:0,
         duration:1,
-        // autoAlpha:1,
+        autoAlpha:1,
         ease: Power4,
     }).to('a',{
         autoAlpha:1,
@@ -25,5 +25,48 @@ function animationNavbar() {
      })
 }());
 
-// svg anime
+// open modale
+const openModale = document.querySelectorAll('.openModale');
+const Modale =  document.querySelector('#modale');
+const closeModale = document.querySelector('#close-btn');
+
+const tiltel = document.querySelector("#title")
+
+
+openModale.forEach(el=>{
+    el.addEventListener('click', ()=>{
+        if(el.dataset.id === "Porject1"){
+            Modale.style.display="block"
+            tiltel.firstChild.data = project["project1 "].nom
+        }
+
+        if(el.dataset.id === "Porject2"){
+            Modale.style.display="block"
+            tiltel.firstChild.data = project["project2 "].nom
+        }
+    })
+})
+
+closeModale.addEventListener('click', ()=>{
+    Modale.style.display="none"
+})
+
+
+let project = {
+    "project1 " : {
+        "nom" : "App git",
+        "Details" : "You can easily create sticky or fixed header and footer using the CSS fixed positioning.",
+        "technos" : "HTML,",
+        "technos2": "Css",
+        "technos3": "js",
+    },
+    "project2 " : {
+        "nom" : "Dereje",
+        "Details" : "You can easily create sticky or fixed header and footer using the CSS fixed positioning.",
+        "technos" : "HTML,",
+        "technos2": "Css",
+        "technos3": "js",
+    }
+}
+
 
