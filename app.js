b6b687fdd523feb9646ee381fc2a360a19288585
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2022-05-04 14:51:13 +0200

    debut style section formulaire

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
+const form = document.querySelector("form");
+const sujet = document.querySelector(".sujet");
+const email = document.querySelector(".email");
+const message = document.querySelector(".text");
 
+form.addEventListener("submit", (e) => {
+  // value
+  let valueSujet = sujet.value;
+  let valueEmail = email.value;
+  let valueMessage = message.value;
 
-let projects = {
-  "project1 ": {
-    nom: "App git",
-    Details:
-      "You can easily create sticky or fixed header and footer using the CSS fixed positioning.",
-    technos: "HTML,",
-    technos2: "Css",
-    technos3: "js",
-  },
-  "project2 ": {
-    nom: "Dereje",
-    Details:
-      "You can easily create sticky or fixed header and footer using the CSS fixed positioning.",
-    technos: "HTML,",
-    technos2: "Css",
-    technos3: "js",
-  },
-};
+  const data = new FormData();
+
+  e.preventDefault();
+  let url = "./form.php";
+
+  fetch(url, {
+    method: "POST",
+    body: data,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+});
