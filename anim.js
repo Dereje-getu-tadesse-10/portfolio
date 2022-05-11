7b4a6aa59456cadb80281e10bb05167e7a6f2f55
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2022-05-11 10:28:30 +0200

    bug menu burger

--- anim.js
+++ anim.js
@@ -1,23 +1,40 @@
 const btn = document.querySelector(".openMenu");
 
 const tl = gsap.timeline();
-function animationNavbar() {
-  tl.to(".navAnimation", {
-    y: 0,
-    autoAlpha: 1,
-    ease: Power4,
-  })
-    .to("a", {
-      autoAlpha: 1,
-      stagger: 0.4,
-      ease: Power2,
-    })
-    .reverse();
-}
+// function animationNavbar() {
+//   tl.to(".navAnimation", {
+//     y: 0,
+//     autoAlpha: 1,
+//     ease: Power4,
+//   })
+//     .to("a", {
+//       autoAlpha: 1,
+//       stagger: 0.4,
+//       ease: Power2,
+//     })
+//     .reverse();
+// }
 
-(function open() {
-  animationNavbar();
-  btn.addEventListener("click", () => {
-    tl.reversed(!tl.reversed());
-  });
-})();
+// (function open() {
+//   animationNavbar();
+//   btn.addEventListener("click", () => {
+//     tl.reversed(!tl.reversed());
+//   });
+// })();
+
+const burger = document.querySelector("#burger");
+
+const tl = gsap.timeline();
+
+tl.to(".menu-link", {
+  translateY: "100%",
+  duration: 0.5,
+});
+
+tl.to(".menu-overlay", {
+  width: "0",
+});
+
+burger.addEventListener("click", () => {
+  tl.reversed(!tl.reversed());
+});
