cbb459ee8a0f59989bdb12920efc7cb6b80df72d
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2022-05-13 11:40:39 +0200

    hehe fin du probleme sur le menu burger

--- anim.js
+++ anim.js
@@ -1,25 +1,14 @@
+const burger = document.querySelector(".openMenu");
+const nav = document.querySelector(".navAnimation");
+const links = document.querySelectorAll(".link");
 
-const btn = document.querySelector(".openMenu");
-
-const tl = gsap.timeline();
-function animationNavbar() {
-  tl.to(".navAnimation", {
-    y: 0,
-    autoAlpha: 1,
-    ease: Power4,
-  })
-    .to("a", {
-      autoAlpha: 1,
-      stagger: 0.4,
-      ease: Power2,
-    })
-    .reverse();
-}
-
-(function open() {
-  animationNavbar();
-  btn.addEventListener("click", () => {
-    tl.reversed(!tl.reversed());
+burger.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  links.forEach((element) => {
+    element.addEventListener("click", () => {
+      if (nav.classList.contains("open")) {
+        nav.classList.remove("open");
+      }
+    });
   });
-})();
-
+});
